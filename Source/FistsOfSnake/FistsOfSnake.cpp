65c1920407b3c301fd70a19a9e4ba95e490f5430
Dominik Frankowski <dominik-frankowski@protonmail.com>
2020-11-15 17:02:22 +0100

    Initial files

---
+++ Source/FistsOfSnake/FistsOfSnake.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FistsOfSnake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FistsOfSnake, "FistsOfSnake" );
---
+++ Source/FistsOfSnake/FistsOfSnakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FistsOfSnakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISTSOFSNAKE_API AFistsOfSnakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
